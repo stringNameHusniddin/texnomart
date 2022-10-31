e37c75ffd9518e213b3df0330e97e589919b2d2c
stringNameHusniddin <stringhusniddin@gmail.com>
2022-10-31 15:10:54 +0500

    texnomart

---
+++ main.js
@@ -0,0 +1,279 @@
+let object = [
+    {
+        name: "Changyutgich Ferre VC 1108C Kulrang",
+        brand: "Ferre",
+        price: "700000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/104915/35419/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/104915/35419/detail.jpg"],
+        type: "Changyutgich"
+    },
+
+    {
+        name: "Changyutgich Ferre VC 1108C Qizil",
+        brand: "Ferre",
+        price: "700000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/104916/35420/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/104916/35420/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Artel VCB0316 Qora",
+        brand: "Artel",
+        price: "770000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80178/5364/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80178/5364/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Artel VCB0316 Qizil",
+        brand: "Artel",
+        price: "770000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80188/5382/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80188/5382/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Artel VCB0316 Oq",
+        brand: "Artel",
+        price: "770000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80195/5393/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80195/5393/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Samsung SC20M255AWB",
+        brand: "Samsung",
+        price: "800000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/86070/20364/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/86070/20364/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Samsung VCC5610",
+        brand: "Samsung",
+        price: "950000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80174/5358/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80174/5358/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Artel VCB0120 GRY",
+        brand: "Artel",
+        price: "954000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/86494/18796/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/86494/18796/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Artel VCB0120 WHT",
+        brand: "Artel",
+        price: "975000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80193/5391/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80193/5391/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Samsung SC18M21C0VN",
+        brand: "Samsung",
+        price: "1 037 000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/104732/35220/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/104732/35220/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Gorenje VC 1611 CXBK",
+        brand: "Gorenje",
+        price: "1073000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80221/5469/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80221/5469/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Samsung SC18M21BOS2",
+        brand: "Samsung",
+        price: "1089000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/104448/34573/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/104448/34573/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich LG VK76A06NDB",
+        brand: "LG",
+        price: "1092000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80158/5323/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80158/5323/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich LG VK76A00NDS",
+        brand: "LG",
+        price: "1145000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80169/5349/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80169/5349/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Samsung VCC5660X3K/XEV",
+        brand: "Samsung",
+        price: "1150000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80202/20436/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80202/20436/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Artel VSS0220 (kulrang)",
+        brand: "Artel",
+        price: "1150000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80190/32751/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80190/32751/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Artel VCC0220 (To`q qizil)",
+        brand: "Artel",
+        price: "1150000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80241/5525/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80241/5525/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich LG VK69662N",
+        brand: "LG",
+        price: "1155000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/91536/19310/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/91536/19310/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich Polaris PVC 2016",
+        brand: "Polaris",
+        price: "1171000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/96738/29660/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/96738/29660/detail.jpg"],
+        type: "Changyutgich"
+    },
+    {
+        name: "Changyutgich LG VK76A06NDR",
+        brand: "LG",
+        price: "1251000",
+        images: ["https://texnomart.uz/_ipx/f_webp/https://backend.texnomart.uz/images/gallery/product/80155/5312/preview.jpg", "https://backend.texnomart.uz/images/gallery/product/80155/5312/detail.jpg"],
+        type: "Changyutgich"
+    },
+
+
+]
+let objectUzunligi = object.length
+for(let i = 0; i<objectUzunligi; i++){
+    let element = object[i]
+    element["id"] = `${i}`
+}
+
+let pName = document.createElement("p")
+pName.setAttribute("class", "pName")
+let pNarx = document.createElement("p")
+pNarx.setAttribute("class", "pNarx")
+let img = document.createElement("img")
+
+
+
+for(let i =0; i<objectUzunligi; i++){
+    let element = object[i];
+    let tez = document.createElement("p")
+    tez.className  ="tezKurish"
+    tez.textContent = "Tezkor ko'rish"
+    let iImg = img.cloneNode(true)
+    iImg.src = element.images[0]
+    let iPName = pName.cloneNode(true)
+    iPName.textContent = element.name
+    let iPNarx = pNarx.cloneNode(true)
+    iPNarx.textContent = `${element.price} so'm`
+    let idP = document.createElement("p")
+    idP.textContent = element.id
+
+
+
+    let card = document.createElement("div")
+    card.className = "card"
+    let card2 = document.createElement("div")
+    card2.className = "card2"
+
+    let bul = parseInt(element.price) / 24
+    let oyTulash =document.createElement("p")
+    oyTulash.className = "oyTulash"
+    oyTulash.textContent = `${parseInt(bul)}dan/24 oy`
+    
+    let btnCard = document.createElement("button")
+    btnCard.className = "btnCard"
+
+    let btnCardP = document.createElement("p")
+    btnCardP.textContent = "savatchaga"
+
+    let btnCardIMg = document.createElement("span")
+    btnCardIMg.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.33301 1.74999C1.33301 1.24373 1.74342 0.833313 2.24969 0.833313H4.71301C5.14707 0.833313 5.52164 1.13774 5.61034 1.56264L6.22362 4.50004H17.8333C18.8458 4.50004 19.6666 5.32086 19.6666 6.3334V10.298C19.6666 11.166 19.0581 11.9149 18.2086 12.0926L8.24725 14.1761C7.25609 14.3833 6.28456 13.7479 6.07735 12.7567L3.96795 2.66668H2.24969C1.74342 2.66668 1.33301 2.25626 1.33301 1.74999ZM7.68189 11.4846C7.78538 11.9803 8.27118 12.2982 8.76684 12.1946L17.1042 10.4513C17.5289 10.3625 17.8333 9.98794 17.8333 9.55398V7.25008C17.8333 6.74382 17.4229 6.3334 16.9166 6.3334H6.6064L7.68189 11.4846Z" fill="#333333"></path><path d="M7.16669 19.1666C8.08718 19.1666 8.83338 18.4204 8.83338 17.4999C8.83338 16.5795 8.08718 15.8333 7.16669 15.8333C6.2462 15.8333 5.5 16.5795 5.5 17.4999C5.5 18.4204 6.2462 19.1666 7.16669 19.1666Z" fill="#333333"></path><path d="M15.4997 19.1666C16.4202 19.1666 17.1664 18.4204 17.1664 17.4999C17.1664 16.5795 16.4202 15.8333 15.4997 15.8333C14.5792 15.8333 13.833 16.5795 13.833 17.4999C13.833 18.4204 14.5792 19.1666 15.4997 19.1666Z" fill="#333333"></path></svg>'
+
+    btnCard.appendChild(btnCardIMg)
+    btnCard.appendChild(btnCardP)
+
+    card.appendChild(tez)
+    card.appendChild(iImg)
+    card2.appendChild(iPName)
+    card2.appendChild(iPNarx)
+    card2.appendChild(oyTulash)
+    card2.appendChild(btnCard)
+    card2.appendChild(idP)
+    
+    idP.style.display = "none"
+    card.appendChild(card2)
+
+    if(document.getElementById("right")){
+        document.getElementById("right").appendChild(card)  
+    }
+}
+for(let i=0; i<objectUzunligi; i++){
+    if(document.getElementById("right")){
+        let btnCard = document.getElementsByClassName("btnCard")
+        btnCard[i].addEventListener("click", (e)=>{
+            let id = e.target.parentElement.parentElement.childNodes[4].textContent
+            console.log(id)
+            localStorage.setItem("id", id)
+            location.assign("./index0.html")
+        })
+    }
+}
+if(document.getElementById("asosiyImg")){
+    let malId = localStorage.getItem("id")
+    let info;
+    let mal = object.map((narsa)=>{
+        if(narsa.id === malId){
+            return narsa
+        }
+    })
+    for (const narsa of mal) {
+        if(narsa){
+            info = narsa
+        }
+    }
+    console.log(info)
+
+    
+        document.getElementById("asosiyImg").src = info.images[1]
+
+        let title = document.getElementById("title")
+        title.textContent = info.name
+
+        let brend = document.getElementById("brend")
+        brend.textContent = info.brand
+
+        let narx = document.getElementById("narx")
+        narx.textContent = info.price
+
+
+        let bul = parseInt(info.price) / 24
+        let oyTulash = document.getElementById("oyTulash")
+        oyTulash.textContent = `${parseInt(bul)}dan/24 oy`
+}
+
+
+
+
+if(document.getElementsByClassName("tezKurish")){
+    document.getElementsByClassName("tezKurish")[0].addEventListener("click", (e)=>{
+        let index = e.target.parentElement.childNodes[2].childNodes[4].textContent;
+        let img =   document.createElement("img")
+        img.id = "asosiyImg"
+        img.src = object[index].images[1]
+        let title = document.createElement("p")
+        title.id = "title"
+        title.textContent = object[index].name
+        let narx = document.createElement("p")
+        narx.textContent = object[index].price
+        narx.id = "narx"
+        let bul = parseInt(object[index].price) / 24
+        let oyTulash = document.getElementById("oyTulash")
+        oyTulash.textContent = `${parseInt(bul)}dan/24 oy`
+        let brend = document.createElement("p")
+        brend.id = "brend"
+        brend.textContent = object[index].brand
+    })
+}
